4eef4460b901901d689909a5eee4fc7b3f273961
RePozitive <Extayle.SM@yandex.ru>
2021-10-05 21:12:22 +0300

    Creating models, schemas

--- Recommendation system/schemas.py
+++ Recommendation system/schemas.py
@@ -1,4 +1,16 @@
 import marshmallow as ma
+from bson import ObjectId
+
+
+class Nested(ma.fields.Nested):
+    """A nested field that deserializes from bson.ObjectId string and serializes to nested object"""
+    def _deserialize(self, value, attr, data, partial=None, **kwargs):
+        return ObjectId(value)
+
+class Authentification_Schema(ma.Schema):
+    login = ma.fields.String()
+    first_password = ma.fields.String()
+    second_password = ma.fields.String()
 
 class Authors_Schema(ma.Schema):
     image_url = ma.fields.String()
@@ -14,10 +26,7 @@ class Composition_Schema(ma.Schema):
     image_url = ma.fields.String()
     name = ma.fields.String()
     date = ma.fields.Date()
-    author = ma.fields.Nested(Authors_Schema)
+    author = Nested("Authors_Schema")
 
-class Authentification_Schema(ma.Schema):
-    login = ma.fields.String()
-    first_password = ma.fields.String()
-    second_password = ma.fields.String()
+
 
