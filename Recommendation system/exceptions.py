ffc429640dd9214879811449e8da9cffd89bee39
RePozitive <Extayle.SM@yandex.ru>
2021-10-08 22:27:26 +0300

    Add new exceptions

--- Recommendation system/exceptions.py
+++ Recommendation system/exceptions.py
@@ -28,3 +28,33 @@ class AbsentCompositionInBd(Exception):
 
         def __str__(self):
             return f'{self.composition_id} -> {self.message}'
+
+class FalseLogin(Exception):
+    '''
+        Исключение возникает в случае, если текущий логин является не уникальным в коллекции Authentification
+        Атрибуты:
+            message — объяснение ошибки. 
+    '''
+    def __init__(self, message = "This login already exists"):
+        self.message = message
+        super().__init__(self.message)
+
+class ErrorPaswords(Exception):
+    '''
+        Исключение возникает в случае, если введённые пароли при регистрации не совпадают.
+        Атрибуты:
+            message — объяснение ошибки.
+    '''
+    def __init__(self, message = "Passwords don't match"):
+        self.message = message
+        super().__init__(self.message)
+
+class ErrorLoginOrPassword(Exception):
+    '''
+        Исключение возникает в случае, если введённые логин или пароль не совпадают при авторизации.
+        Атрибуты:
+            message — объяснение ошибки.
+    '''
+    def __init__(self, message = "The username or password is incorrect"):
+        self.message = message
+        super().__init__(self.message)
