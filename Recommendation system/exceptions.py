1de179f14c852b87802378dcd0992eee52cb612f
RePozitive <Extayle.SM@yandex.ru>
2021-10-11 21:00:06 +0300

    Full update software implementation exceptions

--- Recommendation system/exceptions.py
+++ Recommendation system/exceptions.py
@@ -1,60 +1,55 @@
-class AbsentAuthorInBd(Exception):
-    ''' 
-        Исключение возникает в момент отсутствия искомого объекта в коллекции Author.
-        Атрибуты:
-            author_id — id автора
-            message — объяснение ошибки.
-    '''
+from flask import jsonify
 
-    def __init__(self, author_id, message = "This author is not in database"):
-        self.author_id = author_id
+
+def template(data, code=500):
+    return {'message': {'errors': {'body': data}}, 'status_code': code}
+
+Error_Absent_Author = template(["This author is not in database"], code=404)
+Error_Absent_Composition = template(["This composition is not in database"], code=404)
+User_Already_Registered = template(["This login already exists"], code=422)
+Error_Passwords = template(["Passwords don't match"], code=404)
+Error_Login_or_Password = template(["Passwords don't match"], code=400)
+Registration_Error = template(["Registration is not found"], code=404)
+Authorisation_Error = template(["Authorisation is not found"], code=404)
+
+class InvalidUsage(Exception):
+    status_code = 500
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
         self.message = message
-        super().__init__(self.message)
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
 
-        def __str__(self):
-            return f'{self.author_id} -> {self.message}'
+    def to_json(self):
+        rv = self.message
+        return jsonify(rv)
 
-class AbsentCompositionInBd(Exception):
-    '''
-        Исключение возникает в момент отсутствия искомого объекта в коллекции Composition.
-        Атрибуты:
-            composition_id — id композиции
-            message — объяснение ошибки.
-    '''
-    def __init__(self, composition_id, message = "This composition is not in database"):
-        self.composition_id = composition_id
-        self.message = message
-        super().__init__(self.message)
+    @classmethod
+    def AuthorIsAbsent(cls):
+        return cls(**Error_Absent_Author)
 
-        def __str__(self):
-            return f'{self.composition_id} -> {self.message}'
+    @classmethod
+    def CompositionIsAbsent(cls):
+        return cls(**Error_Absent_Composition)
 
-class FalseLogin(Exception):
-    '''
-        Исключение возникает в случае, если текущий логин является не уникальным в коллекции Authentification
-        Атрибуты:
-            message — объяснение ошибки. 
-    '''
-    def __init__(self, message = "This login already exists"):
-        self.message = message
-        super().__init__(self.message)
+    @classmethod
+    def UserAlreadyRegistered(cls):
+        return cls(**User_Already_Registered)
 
-class ErrorPaswords(Exception):
-    '''
-        Исключение возникает в случае, если введённые пароли при регистрации не совпадают.
-        Атрибуты:
-            message — объяснение ошибки.
-    '''
-    def __init__(self, message = "Passwords don't match"):
-        self.message = message
-        super().__init__(self.message)
+    @classmethod
+    def ErrorPasswords(cls):
+        return cls(**Error_Passwords)
 
-class ErrorLoginOrPassword(Exception):
-    '''
-        Исключение возникает в случае, если введённые логин или пароль не совпадают при авторизации.
-        Атрибуты:
-            message — объяснение ошибки.
-    '''
-    def __init__(self, message = "The username or password is incorrect"):
-        self.message = message
-        super().__init__(self.message)
+    @classmethod
+    def ErrorLoginOrPassword(cls):
+        return cls(**Error_Login_or_Password)
+
+    @classmethod
+    def RegistrationError(cls):
+        return cls(**Registration_Error)
+    
+    @classmethod
+    def AuthorisationError(cls):
+        return cls(**Authorisation_Error)
