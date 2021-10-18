4a69f4ba42a059ae9789c8bd7399a9c9bac1368b
RePozitive <Extayle.SM@yandex.ru>
2021-10-18 17:56:41 +0300

    Added new exceptions

--- Recommendation system/exceptions.py
+++ Recommendation system/exceptions.py
@@ -4,13 +4,18 @@ from flask import jsonify
 def template(data, code=500):
     return {'message': {'errors': {'body': data}}, 'status_code': code}
 
-Error_Absent_Author = template(["This author is not in database"], code=404)
-Error_Absent_Composition = template(["This composition is not in database"], code=404)
+Error_Absent_Author = template(["This author does not exist"], code=404)
+Error_Absent_Composition = template(["This composition does not exist"], code=404)
+Error_Absent_User = template(["This user does not exist"], code=404)
+Error_Role = template(["This role was entered incorrectly"], code=404)
 User_Already_Registered = template(["This login already exists"], code=422)
-Error_Passwords = template(["Passwords don't match"], code=404)
-Error_Login_or_Password = template(["Passwords don't match"], code=400)
-Registration_Error = template(["Registration is not found"], code=404)
-Authorisation_Error = template(["Authorisation is not found"], code=404)
+Error_Passwords = template(["Passwords don't match"], code=401)
+Error_Login_or_Password = template(["Login or Password entered incorrectly"], code=401)
+Registration_Error = template(["Registration is not found"], code=401)
+Authorisation_Error = template(["Authorisation is not found"], code=401)
+Privilege_Admin = template(["You don't have rights to this section"], code=403)
+Privilege_User = template(["To get access, you need to register"], code=403)
+Privilege_SuperAdmin = template(["You don't have rights to this section"], code=403)
 
 class InvalidUsage(Exception):
     status_code = 500
@@ -53,3 +58,23 @@ class InvalidUsage(Exception):
     @classmethod
     def AuthorisationError(cls):
         return cls(**Authorisation_Error)
+
+    @classmethod
+    def AccessAdmin(cls):
+        return cls(**Privilege_Admin)
+
+    @classmethod
+    def AccessUser(cls):
+        return cls(**Privilege_User)
+
+    @classmethod
+    def UserIsAbsent(cls):
+        return cls(**Error_Absent_User)
+
+    @classmethod
+    def IncorrectlyRole(cls):
+        return cls(**Error_Role)
+
+    @classmethod
+    def AccessSuperAdmin(cls):
+        return cls(**Privilege_SuperAdmin)
