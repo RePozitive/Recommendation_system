f3c3b4e26e90195f02364f675334060998bd36cc
Михаил <43346345+RePozitive@users.noreply.github.com>
2021-11-02 00:20:48 +0300

    Update settings.py

--- Recommendation system/settings.py
+++ Recommendation system/settings.py
@@ -1,7 +1,7 @@
 class Config(object):
     DEBUG = False
-    MONGO_URI = "mongodb://localhost:27017/Recomendation_system?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
-    SECRET_KEY = 'ea5c5bf03a1a40938e4eadae8b304bc5'
+    MONGO_URI = URL
+    SECRET_KEY = Your secret key
     MONGODB_SETTINGS = {
         "db": "Recommendation_system",
         "host": MONGO_URI,
